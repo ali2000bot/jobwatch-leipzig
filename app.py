587fab39cd666f2b573a0f2aa4ecbab2cc75c8ee
ali2000bot <lindemann_andre@yahoo.de>
2026-03-02 17:51:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ DEFAULT_NEGATIVE_KEYWORDS = [
     "vorstandsassistenz", "dachdecker", "management assistant", "altenpfleger", "altenpflegehelfer", "staplerfahrer", "restaurantfachmann", 
     "umformmechaniker", "gabelstaplerfahrer", "busfahrer", "elektriker", "schweißer", "mechatroniker", "anlagenführer", "mechaniker", 
     "erzieher", "staplerfahrer", "postbote", "schlosser", "produktionshelfer", "geräteführer", "physiotherapeut", "bauhelfer", "sozialpädagog",
-    "minijob", "junior", "juristisch",
+    "minijob", "junior", "juristisch", "maschinenbediener", "produktionsmtarbeiter", "gießereihelfer", "sachbearbeitung", 
     # Pflege/Gesundheit
     "altenpfleger", "pflege", "krankenpfleger", "pflegefachkraft", "pflegedienst",
     "gesundheits", "medizinische", "arzthelfer", "mfa", "therapeut",
@@ -931,7 +931,7 @@ def is_probably_irrelevant(it: Dict[str, Any]) -> bool:
         "insurance", "versicherung", "steuerfachangestellte", "sachbearbeiter", "gabelstaplerfahrer", 
         "busfahrer", "elektriker", "schweißer", "mechatroniker", "anlagenführer", "mechaniker",
         "erzieher", "staplerfahrer", "postbote", "schlosser", "produktionshelfer", "geräteführer", "physiotherapeut", "bauhelfer",
-        "sozialpädagog", "minijob", "junior", "juristisch",
+        "sozialpädagog", "minijob", "junior", "juristisch", "maschinenbediener", "produktionsmtarbeiter", "gießereihelfer", "sachbearbeitung",
 
         # Pflege/Gesundheit
         "altenpfleger", "pflege", "pflegefachkraft", "krankenpfleger", "pflegedienst",
